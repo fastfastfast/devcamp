be47934f57c20ae4f9e67be0fb9469f7884d0455
sern <sanghit@hotmail.com>
2020-11-30 09:52:33 +0700

    add Play in page, fibase in services, Play.css in styles

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Home';
+import Play from './page/Play';
 
 ReactDOM.render(
   <React.StrictMode>
     <Home />
+    <Play />
   </React.StrictMode>,
   document.getElementById('root')
 );
