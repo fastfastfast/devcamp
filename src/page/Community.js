bbf1009684a1b6dae3c74fece970342a18ebb9de
sern <sanghit@hotmail.com>
2020-12-02 11:50:22 +0700

    edit card size

--- src/page/Community.js
+++ src/page/Community.js
@@ -3,6 +3,7 @@ import Figure from 'react-bootstrap/Figure';
 import { Card, ListGroup, Tab, Row, Col, Form, Button } from 'react-bootstrap';
 import { firebase } from '../services/firebase'
 import { useEffect, useState } from 'react'
+import '../Styles/Community.css';
 
 function Community() {
     const [postInfo, setPostInfo] = useState()
@@ -16,8 +17,7 @@ function Community() {
         // console.log(card.infoPost.name)
         {/*loop display card namd and description*/ }
         return (
-            <Card style={{ width: '18rem' }} key={index} className="box">
-                {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+            <Card style={{ width: '50rem' }} className="post-card">
                 <Card.Body>
                     <Card.Title>{card.infoPost.name}</Card.Title>
                     <Card.Text>{card.infoPost.post}</Card.Text>
@@ -73,69 +73,69 @@ function Community() {
                                     <Form onSubmit={onSubmit}>
                                         <Form.Group controlId="exampleForm.ControlTextarea1">
                                             <Form.Label style={{ marginTop: "20px" }}>Post</Form.Label>
-                                            <Form.Control name="textField"as="textarea" rows={3} />
+                                            <Form.Control name="textField" as="textarea" rows={3} />
                                         </Form.Group>
                                         <Button as="input" type="submit" value="Submit" />
                                     </Form>
-                                    {postInfo && 
-                                            <div> {/* if postInfo have item  */}
-                                                {
-                                                    postInfo.dota2.map(renderCard) 
-                                                        /* loop if user.game have dota2 include in postInfo call renderCard -> postInfo  */
-                                                }
-                                            </div>
-                                        }
+                                    {postInfo &&
+                                        <div> {/* if postInfo have item  */}
+                                            {
+                                                postInfo.dota2.map(renderCard)
+                                                /* loop if user.game have dota2 include in postInfo call renderCard -> postInfo  */
+                                            }
+                                        </div>
+                                    }
                                 </Tab.Pane>
                                 <Tab.Pane eventKey="#csgo">
-                                <Form onSubmit={onSubmit}>
+                                    <Form onSubmit={onSubmit}>
                                         <Form.Group controlId="exampleForm.ControlTextarea1">
                                             <Form.Label style={{ marginTop: "20px" }}>Post</Form.Label>
-                                            <Form.Control name="textField"as="textarea" rows={3} />
+                                            <Form.Control name="textField" as="textarea" rows={3} />
                                         </Form.Group>
                                         <Button as="input" type="submit" value="Submit" />
-                                        </Form>
-                                    {postInfo && 
-                                            <div> {/* if postInfo have item  */}
-                                                {
-                                                    postInfo.csgo.map(renderCard) 
-                                                        /* loop if user.game have csgo include in postInfo call renderCard -> postInfo  */
-                                                }
-                                            </div>
-                                        }
+                                    </Form>
+                                    {postInfo &&
+                                        <div> {/* if postInfo have item  */}
+                                            {
+                                                postInfo.csgo.map(renderCard)
+                                                /* loop if user.game have csgo include in postInfo call renderCard -> postInfo  */
+                                            }
+                                        </div>
+                                    }
                                 </Tab.Pane>
                                 <Tab.Pane eventKey="#amongus">
-                                <Form onSubmit={onSubmit}>
+                                    <Form onSubmit={onSubmit}>
                                         <Form.Group controlId="exampleForm.ControlTextarea1">
                                             <Form.Label style={{ marginTop: "20px" }}>Post</Form.Label>
-                                            <Form.Control name="textField"as="textarea" rows={3} />
+                                            <Form.Control name="textField" as="textarea" rows={3} />
                                         </Form.Group>
                                         <Button as="input" type="submit" value="Submit" />
                                     </Form>
-                                    {postInfo && 
-                                            <div> {/* if postInfo have item  */}
-                                                {
-                                                    postInfo.amongus.map(renderCard) 
-                                                        /* loop if user.game have amongus include in postInfo call renderCard -> postInfo  */
-                                                }
-                                            </div>
-                                        }
+                                    {postInfo &&
+                                        <div> {/* if postInfo have item  */}
+                                            {
+                                                postInfo.amongus.map(renderCard)
+                                                /* loop if user.game have amongus include in postInfo call renderCard -> postInfo  */
+                                            }
+                                        </div>
+                                    }
                                 </Tab.Pane>
                                 <Tab.Pane eventKey="#genshin">
-                                <Form onSubmit={onSubmit}>
+                                    <Form onSubmit={onSubmit}>
                                         <Form.Group controlId="exampleForm.ControlTextarea1">
                                             <Form.Label style={{ marginTop: "20px" }}>Post</Form.Label>
-                                            <Form.Control name="textField"as="textarea" rows={3} />
+                                            <Form.Control name="textField" as="textarea" rows={3} />
                                         </Form.Group>
                                         <Button as="input" type="submit" value="Submit" />
                                     </Form>
-                                    {postInfo && 
-                                            <div> {/* if postInfo have item  */}
-                                                {
-                                                    postInfo.genshin.map(renderCard) 
-                                                        /* loop if user.game have genshin include in postInfo call renderCard -> postInfo  */
-                                                }
-                                            </div>
-                                        }
+                                    {postInfo &&
+                                        <div> {/* if postInfo have item  */}
+                                            {
+                                                postInfo.genshin.map(renderCard)
+                                                /* loop if user.game have genshin include in postInfo call renderCard -> postInfo  */
+                                            }
+                                        </div>
+                                    }
                                 </Tab.Pane>
                             </Tab.Content>
                         </Col>
